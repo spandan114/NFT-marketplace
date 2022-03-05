d9bbac1ee981c1be0bd25a0ba697fcc965e3a660
spandan114 <spandanj685@gmail.com>
2022-03-05 23:18:44 +0530

    Item strucet & events

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # NFT Marketplace
 
 [ERC-721 token details](https://github.com/OpenZeppelin/openzeppelin-contracts/tree/master/contracts/token/ERC721)
+
 [Marketplace contract reference](https://dev.to/dabit3/building-scalable-full-stack-apps-on-ethereum-with-polygon-2cfb)
+
+[another ref](https://github.com/husnn/web3-tutorials/blob/master/02-nft-marketplace/contracts/Market.sol)
 
 ```shell
 npx hardhat accounts
--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -6,10 +6,15 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+// [] Create & List Nft
+// [] Buy NFT
+// [] Sell NFT
+// [] Create buy Order
+// [] Cancel buy Order 
+
 contract NFTMarket is ReentrancyGuard{
     using Counters for Counters.Counter;
     Counters.Counter private _itemId;
-    Counters.Counter private _itemSold;
     
     address payable owner;
     uint256 public mintingCost = 0.001 ether;
@@ -18,8 +23,22 @@ contract NFTMarket is ReentrancyGuard{
         owner = payable(msg.sender);
     }
 
-    struct _Items{
-      uint itemId;
+    struct _Item {
+      uint256 itemId;
+      address payable seller;
+      address payable owner;
+      address token;
+      uint256 price;
     }
+
+    event Item (
+      uint256 indexed itemId,
+      address seller,
+      address owner,
+      address token,
+      uint256 price
+    );
+
+    mapping(uint => _Item) public Items;
 
 }
